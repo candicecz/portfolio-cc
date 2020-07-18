37f11dac413d51bd0c8d6db85db26bf4d81f289f
candicecz <czech.candice@gmail.com>
2020-07-17 20:48:10 -0400

    [UPDATE] service workers

--- public/sw.js
+++ public/sw.js
@@ -3,23 +3,23 @@ const filesToCache = ["index.html"];
 
 self.addEventListener("install", function (event) {
   // Perform install steps
-  console.log("[Servicework] Install");
+  // console.log("[Servicework] Install");
   event.waitUntil(
     caches.open(cacheName).then(function (cache) {
-      console.log("[ServiceWorker] Caching app shell");
+      // console.log("[ServiceWorker] Caching app shell");
       return cache.addAll(filesToCache);
     })
   );
 });
 
 self.addEventListener("activate", function (event) {
-  console.log("[Servicework] Activate");
+  // console.log("[Servicework] Activate");
   event.waitUntil(
     caches.keys().then(function (keyList) {
       return Promise.all(
         keyList.map(function (key) {
           if (key !== cacheName) {
-            console.log("[ServiceWorker] Removing old cache shell", key);
+            // console.log("[ServiceWorker] Removing old cache shell", key);
             return caches.delete(key);
           }
         })
@@ -29,7 +29,7 @@ self.addEventListener("activate", function (event) {
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("[ServiceWorker] Fetch");
+  // console.log("[ServiceWorker] Fetch");
   event.respondWith(
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
